656b76d6d452bf2b8ea24b6a66b461d5692662a7
Augustin-Louis-Xu <GuojiXu2018@163.com>
2018-05-28 12:07:39 +0800

    更新

--- 第三章/练习题.py
+++
@@ -1,5 +0,0 @@
-famous_person='Albert Einstein :' 
-message=" A person who never made a mistake never tried anyting new."
-famous_person_message=famous_person+message
-print(famous_person_message) 
-
