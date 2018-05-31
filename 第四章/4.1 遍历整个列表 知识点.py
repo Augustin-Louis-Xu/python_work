3b90d3c3f16390f3121e1a734e5006138b01ce8a
Augustin-Louis-Xu <GuojiXu2018@163.com>
2018-05-31 10:37:33 +0800

    更新

---
+++ 第四章/4.1 遍历整个列表 知识点.py
@@ -0,0 +1,7 @@
+#在for循环中执行更多的操作，for循环中想要包含多少行循环都可以,可以对列表中的每个值的执行任意次操作
+magicians=['Alice','Tom','David']
+for magician in magicians:
+   print(magician+', that was a great trick')#第一次操作
+   print("I can't wait to see you next magic, "+magician)#第二次操作
+print('Thanks everyone magicians, That was a great magaic show !')#for循环后面没有缩进的代码行，只执行一次。
+#使用for循环是一种很不错的数据方式。譬如说，用for来初始化遍历游戏角色，并且打印到屏幕上，最后添加不缩进的代码块，以用来显示Play now。
